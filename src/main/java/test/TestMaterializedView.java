b1bd85fc3185ccbe601b25c395b8886cd3a725ff
Justin So <sojustin28@gmail.com>
2024-10-29 02:58:20 -0400

    Modify hepPlanner to use MV rewrite rules

--- src/main/java/test/TestMaterializedView.java
+++ src/main/java/test/TestMaterializedView.java
@@ -1,6 +1,7 @@
 package test;
 
 import com.alibaba.fastjson2.JSONArray;
+import com.google.common.collect.ImmutableList;
 import main.Rewriter;
 import main.Utils;
 import org.apache.calcite.config.Lex;
@@ -8,6 +9,7 @@ import org.apache.calcite.plan.*;
 import org.apache.calcite.plan.hep.HepMatchOrder;
 import org.apache.calcite.plan.hep.HepPlanner;
 import org.apache.calcite.plan.hep.HepProgramBuilder;
+import org.apache.calcite.plan.volcano.VolcanoPlanner;
 import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.core.JoinRelType;
 import org.apache.calcite.rel.rel2sql.RelToSqlConverter;
@@ -22,7 +24,9 @@ import org.apache.calcite.tools.FrameworkConfig;
 import org.apache.calcite.tools.Frameworks;
 import org.apache.calcite.tools.RelBuilder;
 import org.apache.calcite.tools.RelBuilderFactory;
+import org.apache.calcite.util.Pair;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -33,21 +37,22 @@ public class TestMaterializedView {
     public static HepProgramBuilder builder(RelOptRule rule_instance) {
         HepProgramBuilder builder = new HepProgramBuilder();
 //        RelOptRule rule_instance = CoreRules.AGGREGATE_EXPAND_DISTINCT_AGGREGATES;
-        builder.addRuleInstance(rule_instance);
+//        builder.addRuleInstance(rule_instance);
 //        List<RelOptRule> t = Arrays.asList(CoreRules.AGGREGATE_PROJECT_MERGE, CoreRules.AGGREGATE_VALUES, PruneEmptyRules.AGGREGATE_INSTANCE);
 //        List<RelOptRule> t = Arrays.asList(CoreRules.FILTER_TO_CALC, CoreRules.PROJECT_TO_CALC, CoreRules.CALC_MERGE);
-//        List<RelOptRule> t = Arrays.asList(
-//                MaterializedViewRules.FILTER_SCAN,
-//                MaterializedViewRules.PROJECT_AGGREGATE,
-//                MaterializedViewRules.PROJECT_FILTER,
-//                MaterializedViewRules.FILTER,
-//                MaterializedViewRules.JOIN,
-//                MaterializedViewRules.PROJECT_JOIN,
-//                MaterializedViewRules.AGGREGATE
-//        );
-//        for (RelOptRule rule : t) {
-//            builder.addRuleInstance(rule);
-//        }
+        List<RelOptRule> t = Arrays.asList(
+                MaterializedViewRules.FILTER_SCAN,
+                MaterializedViewRules.PROJECT_AGGREGATE,
+                MaterializedViewRules.PROJECT_FILTER,
+                MaterializedViewRules.FILTER,
+                MaterializedViewRules.JOIN,
+                MaterializedViewRules.PROJECT_JOIN,
+                MaterializedViewRules.AGGREGATE
+        );
+//        List<RelOptRule> t = SubstitutionVisitor.DEFAULT_RULES;
+        for (RelOptRule rule : t) {
+            builder.addRuleInstance(rule);
+        }
         return builder;
     }
 
@@ -73,6 +78,17 @@ public class TestMaterializedView {
         HepProgramBuilder builder = builder(rule_instance);
         HepPlanner hepPlanner = new HepPlanner(builder.addMatchOrder(HepMatchOrder.TOP_DOWN).build());
         hepPlanner.setRoot(testRelNode);
+
+        String tableSql = "select * from mv0";
+
+        RelNode tableRel = rewriter.SQL2RA(tableSql);
+//        RelNode queryRel = tableRel;
+        RelOptMaterialization mat1 =
+                new RelOptMaterialization(tableRel, testRelNode, null,
+                        ImmutableList.of("default", "mv"));
+        hepPlanner.addMaterialization(mat1);
+
+
 //
 //        ImmutableList<Pair<String, String>> materializationList = ImmutableList.of(Pair.of(testSql, "MV0"));
 //        RelNode testMVRelNode = rewriter.SQL2RA(materializationList.getFirst().left);
@@ -100,7 +116,6 @@ public class TestMaterializedView {
 //                            ImmutableList.of(rewriter.rootSchema.getName(), name)));
 
 
-//        RelOptMaterialization relOptMaterialization = new RelOptMaterialization(testRelNode, testRelNode, null, new ArrayList<>());
 //
 ////        List<RelOptMaterialization> relOptMaterializations = RelOptMaterializations.getApplicableMaterializations(testRelNode, List.of(relOptMaterialization));
 ////        System.out.println(relOptMaterializations.getFirst());
@@ -111,8 +126,8 @@ public class TestMaterializedView {
 //        System.out.println(Objects.requireNonNull(relOptMaterializations.getFirst().left).explain());
 //        System.out.println(Objects.requireNonNull(relOptMaterializations.getFirst().right).getFirst().toString());
 //        System.out.println(Objects.requireNonNull(relOptMaterializations.getFirst().right).getFirst().toString());
-        SqlParser.Config parserConfig = SqlParser.config().withLex(Lex.MYSQL).withUnquotedCasing(UNCHANGED).withCaseSensitive(false).withQuoting(DOUBLE_QUOTE).withConformance(SqlConformanceEnum.MYSQL_5);
-        FrameworkConfig config = Frameworks.newConfigBuilder().parserConfig(parserConfig).defaultSchema(rewriter.rootSchema).build();
+//        SqlParser.Config parserConfig = SqlParser.config().withLex(Lex.MYSQL).withUnquotedCasing(UNCHANGED).withCaseSensitive(false).withQuoting(DOUBLE_QUOTE).withConformance(SqlConformanceEnum.MYSQL_5);
+//        FrameworkConfig config = Frameworks.newConfigBuilder().parserConfig(parserConfig).defaultSchema(rewriter.rootSchema).build();
 
 ////        RelOptMaterialization
 //        RelBuilder relBuilder = RelBuilder.create(config);
@@ -126,19 +141,19 @@ public class TestMaterializedView {
 //        hepPlanner.addMaterialization();
 
 
-//        RelNode rewrite_result = hepPlanner.findBestExp();
-//        System.out.println(testRelNode.explain());
-//        System.out.println(rewrite_result.explain());
-//        if (rewrite_result.deepEquals(testRelNode)) {
-//            System.out.println("No changed!");
-//            return;
-//        }
-//        String rewrite_sql = converter.visitRoot(rewrite_result).asStatement().toSqlString(PostgresqlSqlDialect.DEFAULT).getSql();
-//        System.out.println("TEST SQL =================");
-//        System.out.println(testSql);
-//
-//        System.out.println("REWRITE SQL =================");
-//        System.out.println(rewrite_sql);
+        RelNode rewrite_result = hepPlanner.findBestExp();
+        System.out.println(testRelNode.explain());
+        System.out.println(rewrite_result.explain());
+        if (rewrite_result.deepEquals(testRelNode)) {
+            System.out.println("No changed!");
+            return;
+        }
+        String rewrite_sql = converter.visitRoot(rewrite_result).asStatement().toSqlString(PostgresqlSqlDialect.DEFAULT).getSql();
+        System.out.println("TEST SQL =================");
+        System.out.println(testSql);
+
+        System.out.println("REWRITE SQL =================");
+        System.out.println(rewrite_sql);
 
 
     }
